d3b5fbccfa5e114a116e343a96995d383f87cb3a
HishamSaeed <hisham.saeed.92@gmail.com>
2026-02-11 07:54:46 +0100

    remove default style and added toolbar

---
+++ src/components/tool-bar/ToolBar.tsx
@@ -0,0 +1,11 @@
+import styles from './ToolBar.module.css';
+
+export default function() {
+    return (
+        <main>
+            <div className={styles.container}>
+                
+            </div>
+        </main>
+    )
+}
