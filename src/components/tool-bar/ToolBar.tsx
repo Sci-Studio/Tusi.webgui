7defcf4d56728d336baa5edaca961fcded0811c4
HishamSaeed <hisham.saeed.92@gmail.com>
2026-02-11 08:36:46 +0100

    added push button widget and open and help button

--- src/components/tool-bar/ToolBar.tsx
+++ src/components/tool-bar/ToolBar.tsx
@@ -1,10 +1,18 @@
 import styles from './ToolBar.module.css';
+import PushButton from '../../widgets/push-button/PushButton';
+import help from '../../assets/svg/help.svg';
+import Open from '../../assets/svg/open.svg';
 
 export default function() {
     return (
         <main>
             <div className={styles.container}>
-                
+                <div className={styles.fileContainer}>
+                    <PushButton imageUrl={Open} text='Open'/>
+                </div>
+                <div className={styles.helpContainer}>
+                   <PushButton imageUrl={help} text=''/>
+                </div>
             </div>
         </main>
     )
